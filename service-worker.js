131c7c7cbda2f0f377c7d77de414a492d72440b8
Albert E <einsoft@gmail.com>
2019-03-16 16:22:58 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.56c78564f0d7746860b34b418fa7f9fe.js"
+  "/precache-manifest.1981e685809bc095fd99292d7e44583a.js"
 );
 
 workbox.clientsClaim();
