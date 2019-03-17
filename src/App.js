8baaf75ad655d91e248641f55e57e23acfe173b2
Albert E <einsoft@gmail.com>
2019-03-17 07:42:35 -0300

    Inserção de um componente de terminal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React, { Component } from 'react';
+import Terminal from 'terminal-in-react';
 
-const App = () => <div>
-    <Button variant="outline-danger">Einsoft Microsistemas Integrados</Button>
-</div>;
+class App extends Component {
+    showMsg = () => 'Hello World'
+  
+    render() {
+      return (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh"
+          }}
+        >
+          <Terminal
+            color='green'
+            backgroundColor='black'
+            barColor='black'
+            style={{ fontWeight: "bold", fontSize: "1em" }}
+            commands={{
+              'open-google': () => window.open('https://www.google.com/', '_blank'),
+              showmsg: this.showMsg,
+              popup: () => alert('Terminal in React')
+            }}
+            descriptions={{
+              'open-google': 'opens google.com',
+              showmsg: 'shows a message',
+              alert: 'alert', popup: 'alert'
+            }}
+            msg='You can write anything here. Example - Hello! My name is Foo and I like Bar.'
+          />
+        </div>
+      );
+    }
+  }
 
 export default App;
