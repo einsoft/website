45c6d86721ae78d27dbde6e8edadcb8223e3e819
Albert E <einsoft@gmail.com>
2019-09-14 14:16:54 -0300

    Updates

---
+++ precache-manifest.6d4a68338fee077523c1ee20c8eb4a1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2745dc0d68601e011956949a510637b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0415e2b4b783bc0504b4",
+    "url": "/static/css/main.3f9bc357.chunk.css"
+  },
+  {
+    "revision": "4004e1e7c03502316f64",
+    "url": "/static/js/2.bb4f71ff.chunk.js"
+  },
+  {
+    "revision": "0415e2b4b783bc0504b4",
+    "url": "/static/js/main.bb39c982.chunk.js"
+  },
+  {
+    "revision": "ca5c8fe1c991f76257d4",
+    "url": "/static/js/runtime~main.105f67c5.js"
+  }
+]);
